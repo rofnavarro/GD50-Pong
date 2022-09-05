10e5b764f8e8d9e59bea2bea0d7d08084f94d680
rferrero <rofnavarro@gmail.com>
2022-09-05 20:41:57 -0300

    pong_v2

--- main.lua
+++ main.lua
@@ -1,6 +1,23 @@
+--[[
+	CS50 - GD50 - Pong Full Game
+
+	Author: Colton Ogden
+	cogden@cs50.harvard.edu
+
+	Student: Rodrigo Ferrero
+
+	Pong is originally made in 1972 and is considered
+	the grandfather of games.
+
+	This is a study of the implementation of the game 
+	in lua programming language.
+]]
+
+--	Global Variables
 WINDOW_WIDTH = 1260
 WINDOW_HEIGHT = 720
 
+--	Bootstrap of the game
 function love.load()
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
@@ -9,6 +26,7 @@ function love.load()
 	})
 end
 
+--	Function that deals with all the drawing 
 function love.draw()
 	love.graphics.printf("Hello Pong!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
 end

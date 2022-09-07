a8b529debe14b74b9e6607569c033e6778b816d3
rferrero <rofnavarro@gmail.com>
2022-09-07 20:49:29 -0300

    pong_v5

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 --[[
-	CS50 - GD50 - Pong Full Game
+	CS50 - GD50 - Pong Game
 
 	Student: Rodrigo Ferrero
 
@@ -20,35 +20,39 @@ VIRTUAL_HEIGHT = 243
 --	Requires
 push = require 'push'
 
---	Bootstrap of the game. Initializes the game
+
+--	Game
+
+--	bootstrap of the game, used to nitialize the game
 function love.load()
-	--	Setting filter to point blank
+	--	setting filter to point blank, to take out the blurry from virtualization
 	love.graphics.setDefaultFilter('nearest', 'nearest')
-	--	Setting the retro window
+	--	setting the virtualization of the window, to make it look like old SNES
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
 		resizable = false,
 		vsync = true
 	})
 end
-
+--	function to verify if the 'esc' key os pressed to close the game
 function love.keypressed(key)
 	if key =='escape' then
 		love.event.quit()
 	end
 end
 
---	Function that deals with all the drawing 
+--	function that deals with all the drawing 
 function love.draw()
-	--	Push must be aplly, like a state machine 
+	--	push virtualization initialized
 	push:apply('start')
 	
+	--	print to screen
 	love.graphics.printf("Hello Pong!",				--	text to render
 						 0,							--	starting "x"
 						 VIRTUAL_HEIGHT / 2 - 6,	--	starting "y"
 						 VIRTUAL_WIDTH,				--	number of pixels to center within
 						 'center')					--	alignment 
 
-	--	Push must be apply at the end, like a change on a state machine
+	--	push virtualization must switch to end state
 	 push:apply('end')
 end

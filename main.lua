69be48a162b373c2d7d51d850e91d4771f0cbac6
rferrero <rofnavarro@gmail.com>
2022-09-07 21:07:52 -0300

    pong_v6

--- main.lua
+++ main.lua
@@ -27,6 +27,9 @@ push = require 'push'
 function love.load()
 	--	setting filter to point blank, to take out the blurry from virtualization
 	love.graphics.setDefaultFilter('nearest', 'nearest')
+	--	setting the new font
+	smallfont = love.graphics.newFont('font.ttf', 8)
+	love.graphics.setFont(smallfont)
 	--	setting the virtualization of the window, to make it look like old SNES
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
@@ -46,10 +49,22 @@ function love.draw()
 	--	push virtualization initialized
 	push:apply('start')
 	
+	--	draw the background in gray
+	love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
+	--	draw the ball
+	love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+	
+	--	draw the paddle 1
+	love.graphics.rectangle('fill', 5, 20, 5, 20)
+
+	--	draw the paddle 2
+	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 40, 5, 20)
+
 	--	print to screen
 	love.graphics.printf("Hello Pong!",				--	text to render
 						 0,							--	starting "x"
-						 VIRTUAL_HEIGHT / 2 - 6,	--	starting "y"
+						 20,						--	starting "y"
 						 VIRTUAL_WIDTH,				--	number of pixels to center within
 						 'center')					--	alignment 
 
